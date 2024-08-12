32c9cea42756a07072b07c1dd5f441aa844a86b9
f1rssov <hipsterlegendary14@gmail.com>
2024-08-12 21:53:17 +0300

    project added

---
+++ ft_inception/complete/srcs/requirements/bonus/vsftpd/tools/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+useradd -m -s /bin/bash $FTP_USER > /dev/null
+echo "${FTP_USER}:${FTP_PASS}" | chpasswd > /dev/null
+echo ${FTP_USER} >> /etc/vsftpd.userlist
+echo "***START FTP***"
+# try
+exec $@
+# exec /usr/sbin/vsftpd /etc/vsftpd.conf
